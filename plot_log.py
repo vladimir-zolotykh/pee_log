564d5086f3f37eaea14b0e837f1fcda2d8fdacea
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-01-05 08:50:08 +0200

    Add alias dt for datetime
    +import datetime as dt

--- plot_log.py
+++ plot_log.py
@@ -4,15 +4,17 @@
 import argcomplete
 import argparse
 import sqlite3
-import datetime
+import datetime as dt
 import matplotlib.pyplot as plt
 
 
-def count_1hour_logs(conn, req_dt: datetime.datetime):
+def count_1hour_logs(conn, req_dt: dt.datetime):
     """Count all the logs recorded at the REQ_DT.hour"""
     query, fmt = 'SELECT pee_time FROM pee_log', '%Y-%m-%d %H:%M:%S'
     return sum(1 for row in conn.execute(query)
-               if datetime.datetime.strptime(row[0], fmt).hour == req_dt.hour)
+               if (dt.datetime.strptime(row[0], fmt).hour == req_dt.hour
+                   and (dt.datetime.strptime(row[0], fmt).minute % TICK
+                        == req_dt.minute)))
 
 
 parser = argparse.ArgumentParser(
@@ -21,7 +23,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--db-file', help='Db file', default='pee_log.db')
 parser.add_argument(
     '--date', help='Plot samples of that date',
-    type=datetime.date.fromisoformat, default='2023-12-30')
+    type=dt.date.fromisoformat, default='2023-12-30')
 
 
 NPARTS = 24 * 4
@@ -33,10 +35,9 @@ if __name__ == '__main__':
         x = list(range(NPARTS))
         y = [0] * NPARTS
         for h in range(NPARTS):
-            time = datetime.time(h // 60, h % 60, 0)
-            print(f'{time = }')
+            time = dt.time(h // 60, h % 60, 0)
             y[h] = count_1hour_logs(conn,
-                                    datetime.datetime.combine(args.date, time))
+                                    dt.datetime.combine(args.date, time))
         plt.bar(range(NPARTS), y, color='blue', alpha=0.7)
         plt.xlabel('Hour of the day')
         plt.ylabel('Pees')
