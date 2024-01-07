33b1fb0208170b1b48c3a5e88802f82f744d5f04
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-01-07 09:28:13 +0200

    Add --ignore-duplicates
    added UNIQUE constraint for pee_time

--- read_log.py
+++ read_log.py
@@ -15,17 +15,23 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     '--log-file', help='Log file path', default='pee_log.txt')
 parser.add_argument('--db-file', help='Db file', default='pee_log.db')
+# Default behavior is to raise an exception when a duplicated pee_time
+# found. When --ignore-duplicates is present, all duplicated records
+# are silently ignored.
+parser.add_argument(
+    '--ignore-duplicates', action='store_true',
+    help='Silently ignore (do not insert) duplicated records')
 
 if __name__ == '__main__':
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
-    if os.path.exists(args.db_file):
-        raise sqlite3.OperationalError(f"{args.db_file}: already exists")
+    # if os.path.exists(args.db_file):
+    #     raise sqlite3.OperationalError(f"{args.db_file}: already exists")
     with sqlite3.connect(args.db_file) as conn:
         conn.execute('''
             CREATE TABLE IF NOT EXISTS pee_log (
                 id INTEGER PRIMARY KEY,
-                pee_time TEXT)
+                pee_time TEXT UNIQUE)
         ''')
         with open(args.log_file) as log_file:
             for line_no, line_str in enumerate(log_file.readlines()):
@@ -38,7 +44,13 @@ if __name__ == '__main__':
                     hour, minute = int(line[:2]), int(line[2:])
                     pee_time = datetime.datetime(year, month, day,
                                                  hour, minute)
-                    conn.execute('''
-                        INSERT INTO pee_log (pee_time)
-                        VALUES (?)
-                    ''', (pee_time, ))
+                    try:
+                        conn.execute('''
+                            INSERT INTO pee_log (pee_time)
+                            VALUES (?)
+                        ''', (pee_time, ))
+                    except sqlite3.IntegrityError as e:
+                        if args.ignore_duplicates:
+                            pass
+                        else:
+                            raise e
