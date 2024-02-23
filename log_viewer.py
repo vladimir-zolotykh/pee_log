5fc807b110464176d58d284657dd9087a2f23518
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-02-23 20:24:22 +0200

    Make form in a loop

--- log_viewer.py
+++ log_viewer.py
@@ -10,6 +10,12 @@ class LogViewer(tk.Tk):
         ('640', '2024-01-26 13:31:00', '581', ''),
         ('641', '2024-01-26 14:00:00', '706', '')
     ]
+    fields = [
+        'Date',
+        'Label',
+        'Volume',
+        'Note',
+    ]
 
     def __init__(self, *args, **kwargs):
         super().__init__()
@@ -22,14 +28,19 @@ class LogViewer(tk.Tk):
             log_list.insert(tk.END, line)
         form = tk.Frame(self)
         form.grid(column=1, row=0, sticky=tk.N)
-        label = tk.Label(form, text='Label')
-        label.grid(column=0, row=0)
-        label_entry = tk.Entry(form)
-        label_entry.grid(column=1, row=0)
-        volume_label = tk.Label(form, text='Volume')
-        volume_label.grid(column=0, row=1)
-        volume_value = tk.Entry(form)
-        volume_value.grid(column=1, row=1)
+        for row, fld in enumerate(self.fields):
+            _ = tk.Label(form, text=fld)
+            _.grid(column=0, row=row)
+            _ = tk.Entry(form)
+            _.grid(column=1, row=row)
+        # label = tk.Label(form, text='Label')
+        # label.grid(column=0, row=0)
+        # label_entry = tk.Entry(form)
+        # label_entry.grid(column=1, row=0)
+        # volume_label = tk.Label(form, text='Volume')
+        # volume_label.grid(column=0, row=1)
+        # volume_value = tk.Entry(form)
+        # volume_value.grid(column=1, row=1)
 
 
 if __name__ == '__main__':
