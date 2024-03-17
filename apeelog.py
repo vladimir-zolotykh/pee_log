6d6fa82b13aba8b8b2d8232dfc1a0410e9e84098
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-03-17 19:36:13 +0200

    Bugfix, add pee_log fields

--- apeelog.py
+++ apeelog.py
@@ -21,27 +21,28 @@ class Event(Base):
     __tablename__ = "event"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-    fullname: Mapped[Optional[str]]
+    text: Mapped[str]
     records: Mapped[List["Record"]] = relationship(
-        back_populates="user", cascade="all, delete-orphan"
+        back_populates="event", cascade="all, delete-orphan"
     )
 
     def __repr__(self) -> str:
-        return (f"Event(id={self.id!r}, name={self.name!r}, "
-                f"fullname={self.fullname!r})")
+        return (f"Event(id={self.id!r}, text={self.text!r})")
 
 
 class Record(Base):
     __tablename__ = "record"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email_address: Mapped[str]
+    stamp: Mapped[str]
+    volume: Mapped[Optional[int]]
+    note: Mapped[Optional[str]]
     event_id: Mapped[int] = mapped_column(ForeignKey("event.id"))
     event: Mapped["Event"] = relationship(back_populates="records")
 
     def __repr__(self) -> str:
-        return f"Record(id={self.id!r}, email_address={self.email_address!r})"
+        return (f"Record(id={self.id!r}, stamp={self.stamp!r}, "
+                f"volume={self.volume!r}, note={self.note!r})")
 
 
 engine = create_engine("sqlite://", echo=True)
