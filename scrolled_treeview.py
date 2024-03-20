66eaad76ee9bdbcb1bc8ad9459b00450121616c0
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-03-20 08:53:19 +0200

    Initial import
    A ScrolledTreeview class. Switching to use the Treeview class instead of
    the Listbox for showing the pee log database entries.

--- scrolled_treeview.py
+++ scrolled_treeview.py
@@ -16,7 +16,25 @@ class ScrolledTreeview(ttk.Treeview):
         hbar = tk.Scrollbar(box, orient=tk.HORIZONTAL, command=self.xview)
         hbar.grid(column=0, row=1, stick=tk.EW)
         kwds.update({'yscrollcommand': vbar.set, 'xscrollcommand': hbar.set})
-        super(ScrolledTreeview, self).__init__(parent, **kwds)
+        super(ScrolledTreeview, self).__init__(box, **kwds)
         self.grid(column=0, row=0, sticky=tk.NSEW)
         for m in ('grid', 'grid_configure', 'grid_forget'):
-            self.setattr(m, getattr(box, m))
+            setattr(self, m, getattr(box, m))
+        self.set_columns()
+
+    def set_columns(self):
+        try:
+            flds = list(LogRecord.__fields__)
+        except NameError:
+            flds = ['id', 'stamp', 'label1', 'label2', 'label3', 'volume',
+                    'note']
+        self.configure(columns=flds[1:])
+        self.heading("#0", text=flds[0])
+        for f in flds[1:]:
+            self.heading(f, text=f)
+        self.insert(
+            "",
+            tk.END,
+            text="1",
+            values=('2024-03-19 20:49', 'pee', None, None, 123, 'a note')
+        )
