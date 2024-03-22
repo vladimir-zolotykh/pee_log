c33d8cdff9ec002a18bcef405bf49b0c2d1c8697
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-03-22 20:57:21 +0200

    Refactor ComboDb to use SQLAlchemy

--- combo_db.py
+++ combo_db.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-import tkinter as tk
 from tkinter import ttk
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from apeelog2 import Event
 
 
-class ComboVar(tk.StringVar):
-    def __init__(self, db_con):
-        self.db_con = db_con
-        super().__init__()
+# class ComboVar(tk.StringVar):
+#     def __init__(self, db_con):
+#         self.db_con = db_con
+#         super().__init__()
 
-    def get(self):
-        val = super().get()
-        return val
+#     def get(self):
+#         val = super().get()
+#         return val
 
 
 class ComboDb(ttk.Combobox):
     values_default = ['pee', 'IMET', 'Creatine', 'Coffee', 'headache', 'other']
 
-    def __init__(self, parent, db_con, **kwargs):
-        self.parent = parent    # LogViewer obj
-        self.db_con = db_con    # where is the labels table
-        if 'values' not in kwargs:
-            kwargs.update({'values': self.values_default})
-        super(ComboDb, self).__init__(parent, **kwargs)
+    def __init__(self, parent, engine, **kwds):
+        self.parent, self.engine = parent, engine
+        super(ComboDb, self).__init__(parent, values=self.values_default,
+                                      **kwds)
+        self.update_values()
 
-    def update_values(self):
-        sql = '''
-            SELECT *
-            FROM labels
-            ORDER by label
+    def update_values(self) -> None:
+        with Session(self.engine) as session:
+            values = [event.text for event in session.scalars(select(Event))]
+        self.config(values=values)
 
-        '''
-        labels_dict = {id: lab for id, lab in self.db_con.execute(sql)}
-        self.config(values=list(labels_dict.values()))
+    # def update_values(self):
+    #     sql = '''
+    #         SELECT *
+    #         FROM labels
+    #         ORDER by label
+
+    #     '''
+    #     labels_dict = {id: lab for id, lab in self.db_con.execute(sql)}
+    #     self.config(values=list(labels_dict.values()))
