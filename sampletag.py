4aa344da50e69c857a13e8cb776bce518058a34c
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-03-30 08:29:15 +0200

    Add print_tables, argparse
    Fixed bugs, added __repr__ for Sample, Tag, argparse. Segregated
    initialize (initial pupulating sampletag.db). Added print_tables()
    function.

--- sampletag.py
+++ sampletag.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 from sqlalchemy import Column, Integer, String, ForeignKey, Table, \
-    create_engine
+    create_engine, select
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+import argparse
+import argcomplete
 Base = declarative_base()
 
 
@@ -22,6 +24,11 @@ class Sample(Base):
     text = Column(String)
     tags = relationship('Tag', secondary=sample_tag, back_populates='samples')
 
+    def __repr__(self) -> str:
+        return (f'Samples(id={self.id!r}, time={self.time!r}, '
+                f'volume={self.volume!r}, text={self.text!r}) '
+                f'tags={self.tags!r}')
+
 
 class Tag(Base):
     __tablename__ = 'tag'
@@ -30,9 +37,17 @@ class Tag(Base):
     samples = relationship('Sample', secondary=sample_tag,
                            back_populates='tags')
 
+    def __repr__(self) -> str:
+        return f'Tag(id={self.id!r}, text=={self.text!r})'
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///sampletag.db', echo=True)
+
+parser = argparse.ArgumentParser(
+    description='Initialize/print sampletag.db',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('command', type=str, choices=('init', 'print'))
+
+
+def initialize(engine):
     Base.metadata.create_all(engine)
     pee = Tag(text='pee')
     stool = Tag(text='stool')
@@ -41,8 +56,26 @@ if __name__ == '__main__':
     s2 = Sample(time='2024-03-29 20:00:20', volume=456)
     s1.tags.extend((pee, creatine))
     s2.tags.append(stool)
-
     Session = sessionmaker(engine)
     with Session() as session:
         session.add_all(s1, s2)
         session.commit()
+
+
+def print_tables(engine):
+    Session = sessionmaker(engine)
+    with Session() as session:
+        for sample in session.scalars(select(Sample)):
+            print(sample)
+
+
+if __name__ == '__main__':
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+    engine = create_engine('sqlite:///sampletag.db', echo=False)
+    if args.command == 'init':
+        initialize(engine)
+    elif args.command == 'print':
+        print_tables(engine)
+    else:
+        pass
