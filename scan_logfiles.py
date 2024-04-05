5d0dd0624979c4cc450457371c276eadf6b6c8c3
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-04-05 20:44:55 +0300

    Add replace_2400, has_2400 functions

--- scan_logfiles.py
+++ scan_logfiles.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 import os
+import re
 import glob
 import argparse
+import shutil
+from datetime import datetime
 import argcomplete
 from sampletag_re import logrecords_generator
 
@@ -16,10 +19,47 @@ parser.add_argument(
     '--logdir', default='./LOG_DIARY',
     help='The directory with YYYY-MM-DD.txt logfiles')
 
+
+def has_2400(logfile: str, prefix: str = '2400') -> bool:
+    with open(logfile) as f:
+        for line in (line.strip() for line in f.readlines()):
+            if line.startswith(prefix):
+                return True
+        return False
+
+
+def replace_2400(logfile, backup_ext='.bak'):
+    if backup_ext:
+        shutil.copyfile(logfile, logfile + backup_ext)
+    with open(logfile) as f:
+        lines = f.readlines()
+
+    def get_time4(index: int) -> datetime:
+        m = re.match(r'^(?P<stamp>\d{4}).*$', lines[index])
+        if m:
+            return datetime.strptime(m.group('stamp'), '%H%M')
+        else:
+            raise ValueError(f'{line[index]}: Invalide sample')
+
+    lines2 = []
+    for line_no, line in enumerate(lines):
+        if line[:4] == '2400':
+            t1 = get_time4(line_no - 1)
+            t2 = get_time4(line_no + 1)
+            mid = (t2 - t1) / 2
+            line = datetime.strftime(t1 + mid, '%H%M') + line[4:]
+        lines2.append(line)
+
+    with open(logfile, 'w') as f:
+        f.writelines(lines2)
+
+
 if __name__ == '__main__':
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
     for logname in sorted(glob.glob(os.path.join(args.logdir, '*.txt'))):
+        if has_2400(logname):
+            replace_2400(logname, backup_ext='bak')
         for rec in logrecords_generator(logname):
             if rec.has_tags:
                 print(f'{logname}')
