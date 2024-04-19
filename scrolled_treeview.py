66ada5376063c77ddfc3012bef96f2472af198a1
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-04-19 19:17:49 +0300

    Refactor sort_column()
    Use sorted(..., key=) key arg appropriate to volume type.

--- scrolled_treeview.py
+++ scrolled_treeview.py
@@ -56,7 +56,17 @@ class ScrolledTreeview(ttk.Treeview):
 
         # [(val1, iid1), (val2, iid2), ...]
         values = [(self.set(iid, cid), iid) for iid in self.get_children('')]
-        values.sort(reverse=reverse)
+        if cid in [f'label{i}' for i in range(1, 4)] + ['note']:
+            # tup: (val, cid)
+            key = lambda tup: tup[0]  # noqa
+        elif cid == 'volume':
+            key = lambda tup: int(tup[0])  # noqa
+        elif cid == 'stamp':
+            key = (lambda tup: datetime.strptime(  # noqa
+                tup[0], '%Y-%m-%d %H:%M:%S'))
+        else:
+            key = None
+        values = sorted(values, reverse=reverse, key=key)
         for index, (val, iid) in enumerate(values):
             self.move(iid, '', index)
         self.heading(cid, command=lambda: self.sort_column(cid, not reverse))
@@ -65,7 +75,7 @@ class ScrolledTreeview(ttk.Treeview):
         flds = list(LogRecord.__fields__)
         self.configure(columns=flds[1:])
         self.heading("#0", text=flds[0])
-        w = 5 * 8
+        w = 7 * 8
         self.column("#0", minwidth=w, width=w)
         for f in flds[1:]:
             self.heading(f, text=f, command=(lambda cid=f: self.sort_column(
