699368adfedde352679f6b80b5e94cc6b2e314f4
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-04-27 09:54:39 +0300

    Initial import
    Classes SingletonFont, ButtonFont, TooltipFont
    Usage:
    font = button_font.TooltipFont()
    font=button_font.TooltipFont()
    each font class instance is created only once.

---
+++ button_font.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+"""Usage:
+font = button_font.TooltipFont()
+font=button_font.TooltipFont()"""
+import tkinter.font as tkFont
+
+
+class SingletonFont(tkFont.Font):
+    _instances = {}
+
+    def __new__(cls):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__new__(cls)
+        return cls._instances[cls]
+
+    def __init__(self, family='sans-serif', size=8):
+        super().__init__(family=family, size=size)
+
+
+class ButtonFont(SingletonFont): pass  # noqa
+class TooltipFont(SingletonFont): pass  # noqa
