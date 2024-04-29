620eecd1c2877ceb56c19b6237380f351147d776
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-04-29 20:52:29 +0300

    Import func, UniqueConstraint on separate line
    +from sqlalchemy import UniqueConstraint, func  # noqa

--- sampletag.py
+++ sampletag.py
@@ -7,7 +7,8 @@ from contextlib import contextmanager
 from typing import List, Optional
 from sqlalchemy import (
     Column, Integer, String, ForeignKey, Table, create_engine, select,
-    UniqueConstraint, func)
+    UniqueConstraint)
+from sqlalchemy import UniqueConstraint, func  # noqa
 from sqlalchemy.orm import (
     declarative_base, relationship, sessionmaker, Session)
 from sqlalchemy.exc import SQLAlchemyError
