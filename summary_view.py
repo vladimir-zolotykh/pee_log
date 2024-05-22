a7e3b62894274889bd6b8356561c1c6747aa4fbe
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-05-22 21:47:42 +0300

    Import ScrolledTreeview
    From the new scrolled_treeview module.

--- summary_view.py
+++ summary_view.py
@@ -6,25 +6,8 @@ from dataclasses import dataclass, asdict, field
 from typing import Optional, List
 from datetime import datetime
 import tkinter as tk
-from tkinter import ttk
 import database as db
-
-
-class ScrolledTreeview(ttk.Treeview):
-    def __init__(self, parent, **kwds):
-        box = tk.Frame(parent)
-        box.grid(column=0, row=0, sticky=tk.NSEW)
-        box.columnconfigure(0, weight=1)
-        box.rowconfigure(0, weight=1)
-        vbar = tk.Scrollbar(box, orient=tk.VERTICAL, command=self.yview)
-        vbar.grid(column=1, row=0, sticky=tk.NS)
-        hbar = tk.Scrollbar(box, orient=tk.HORIZONTAL, command=self.xview)
-        hbar.grid(column=0, row=1, stick=tk.EW)
-        kwds.update({'yscrollcommand': vbar.set, 'xscrollcommand': hbar.set})
-        super(ScrolledTreeview, self).__init__(box, **kwds)
-        self.grid(column=0, row=0, sticky=tk.NSEW)
-        for m in ('grid', 'grid_configure', 'grid_forget'):
-            setattr(self, m, getattr(box, m))
+from scrolled_treeview import ScrolledTreeview
 
 
 @dataclass
