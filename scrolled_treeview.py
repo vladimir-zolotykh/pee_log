ce9059df7cf63b276ab5fe21bf834733e9e2025b
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-05-22 21:38:39 +0300

    Initial import
    Is used in detail_view and summary_view. Made a standalone file.
    Originally the code was in scrolled_treeview module from which it was
    segregated and the module renamed to detail_view

---
+++ scrolled_treeview.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import tkinter as tk
+from tkinter import ttk
+
+
+class ScrolledTreeview(ttk.Treeview):
+    def __init__(self, parent, **kwds):
+        box = tk.Frame(parent)
+        box.grid(column=0, row=0, sticky=tk.NSEW)
+        box.columnconfigure(0, weight=1)
+        box.rowconfigure(0, weight=1)
+        vbar = tk.Scrollbar(box, orient=tk.VERTICAL, command=self.yview)
+        vbar.grid(column=1, row=0, sticky=tk.NS)
+        hbar = tk.Scrollbar(box, orient=tk.HORIZONTAL, command=self.xview)
+        hbar.grid(column=0, row=1, stick=tk.EW)
+        kwds.update({'yscrollcommand': vbar.set, 'xscrollcommand': hbar.set})
+        super(ScrolledTreeview, self).__init__(box, **kwds)
+        self.grid(column=0, row=0, sticky=tk.NSEW)
+        for m in ('grid', 'grid_configure', 'grid_forget'):
+            setattr(self, m, getattr(box, m))
