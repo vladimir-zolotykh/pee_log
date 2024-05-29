4d24ce252fdee7731f8498715256b8ad3a465dd4
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-05-29 19:07:19 +0300

    Refactor read_config(), write_config() functions
    Added some comments and global constants: LOG_DIARY_DIR, CONFIG_FILENAME

--- log_diary_config.py
+++ log_diary_config.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+import os
+from typing import Dict
 import csv
+LOG_DIARY_DIR = './LOG_DIARY'
 CONFIG_FILENAME = 'log_diary.csv'
+# Example of the config date.
+# If the file is present in the CONFIG_DATA, e.g., 2024-05-24.txt, it
+# shall be added to the db with the command:
+# $ python wclog.py add --pee-optional 2024-05-24.txt
+
 CONFIG_DATA = [
-    {'log_filename': '2024-05-21.txt', 'options': '--pee-optional'},
-    {'log_filename': '2024-05-24.txt', 'options': '--pee-optional'},
-    {'log_filename': '2024-05-25.txt', 'options': '--pee-optional'},
-    {'log_filename': '2024-05-26.txt', 'options': '--pee-optional'}
-]
+    ['2024-05-21.txt', '--pee-optional'],
+    ['2024-05-24.txt', '--pee-optional'],
+    ['2024-05-25.txt', '--pee-optional'],
+    ['2024-05-26.txt', '--pee-optional']]
 
 
-def write_config():
-    with open(CONFIG_FILENAME, mode='w', newline='') as csvfile:
-        fieldnames = ['log_filename', 'options']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+def write_config(config_filename=CONFIG_FILENAME):
+    """Write the LOG_DIARY config.csv
+
+    A line example:
+    2024-05-21.txt --pee-optional
+    """
+
+    with open(os.path.join(LOG_DIARY_DIR, config_filename),
+              mode='w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=' ')
         writer.writerows(CONFIG_DATA)
 
 
-def read_config():
-    with open(CONFIG_FILENAME, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        print(list(reader))
-        # for row in reader:
-        #     print(row['log_filename'], row['options'])
+def read_config(config_filename=CONFIG_FILENAME) -> Dict[str, str]:
+    """Read the LOG_DIARY config.csv
+
+    Return it as a dict. The dict item example:
+    '2024-05-21.txt': '--pee-optional'
+    """
+
+    with open(os.path.join(LOG_DIARY_DIR, config_filename),
+              newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ')
+        config_dict: Dict[str, str] = {}
+        for row in reader:
+            config_dict[row[0]] = row[1]
+        return config_dict
