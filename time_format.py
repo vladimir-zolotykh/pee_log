bfc59d6b2d75df2345b3665a0a867ef7e310e579
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-10-23 20:10:09 +0300

    Looks the doctest has passed

--- time_format.py
+++ time_format.py
@@ -7,30 +7,34 @@
            '351', '437', '520', '643', '752', '952'])
 ['0125', '0411', '0538', '0710', '0804', '0931', '1050', '1141',\
  '1244', '1302', '1329', '1400', '1423', '1442', '1500', '1531',\
- '1551', '1637', '1720', '1843', '1952', '2152'])
+ '1551', '1637', '1720', '1843', '1952', '2152']
 """
 from datetime import datetime
 
 
 def to24H(times: list[str]) -> list[str]:
-    times_12_hour: list[str] = times
-    times_24_hour: list[str] = []
-
-    for time_str in times_12_hour:
+    times12H: list[str] = times
+    times24H: list[str] = []
+    sfx = 'AM'
+    am0 = datetime.strptime('1200am', '%I%M%p')
+    dt: datetime = am0
+    for time_str in times12H:
         if len(time_str) <= 3:
             time_str = time_str.zfill(4)
-        hour_minute_format = '%I%M'  # %I for 12-hour format hour
-        dt = datetime.strptime(time_str, hour_minute_format)
-        # Determine if it is AM or PM based on time order in the day
-        if (dt.hour < 12 and len(times_24_hour) > 0 and
-                int(times_24_hour[-1][:2]) >= 12):
-            # Convert to PM if it's after 12:00 PM (since the hour
-            # rolls over)
-            print(f'{dt = }, {dt.hour = }')
-            dt = dt.replace(hour=dt.hour + 12)
+        time_str += sfx
+        format = '%I%M%p'
+        dt_prev: datetime = dt
+        dt = datetime.strptime(time_str, format)
+        if dt.hour < dt_prev.hour:
+            sfx = 'PM'
+            time_str = time_str[:-2] + sfx
+            dt = datetime.strptime(time_str, format)
+        # if (dt.hour < 12 and len(times24H) > 0 and
+        #         int(times24H[-1][:2]) >= 12):
+        #     dt = dt.replace(hour=dt.hour + 12)
 
-        times_24_hour.append(dt.strftime('%H%M'))
-    return times_24_hour
+        times24H.append(dt.strftime('%H%M'))
+    return times24H
 
 
 def convert_12_to_24_hour_format(
