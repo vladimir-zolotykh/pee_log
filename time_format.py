6a456f043cca81a329bf87b6af56c928f5d12d73
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-10-23 20:21:35 +0300

    Remove unused code

--- time_format.py
+++ time_format.py
@@ -29,45 +29,8 @@ def to24H(times: list[str]) -> list[str]:
             sfx = 'PM'
             time_str = time_str[:-2] + sfx
             dt = datetime.strptime(time_str, format)
-        # if (dt.hour < 12 and len(times24H) > 0 and
-        #         int(times24H[-1][:2]) >= 12):
-        #     dt = dt.replace(hour=dt.hour + 12)
-
         times24H.append(dt.strftime('%H%M'))
     return times24H
-
-
-def convert_12_to_24_hour_format(
-        file_path: str
-) -> list[str]:
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-
-    date_line = lines[0].strip()
-    print(f"Date: {date_line}")
-
-    times_12_hour: list[str] = [line.strip() for line in lines[1:]]
-    times_24_hour = list[str] = []
-
-    for time_str in times_12_hour:
-        # Ensure time string is 4 digits by padding hour if necessary
-        if len(time_str) <= 3:
-            # Adds leading zero to make hour always 2 digits (e.g.,
-            # '125' -> '0125')
-            time_str = time_str.zfill(4)
-        hour_minute_format = '%I%M'  # %I for 12-hour format hour
-        dt = datetime.strptime(time_str, hour_minute_format)
-        # Determine if it is AM or PM based on time order in the day
-        if (dt.hour < 12 and len(times_24_hour) > 0 and
-                int(times_24_hour[-1][:2]) >= 12):
-            # Convert to PM if it's after 12:00 PM (since the hour
-            # rolls over)
-            dt = dt.replace(hour=dt.hour + 12)
-
-        times_24_hour.append(dt.strftime('%H%M'))
-    for time_24 in times_24_hour:
-        print(time_24)
-    return times_24_hour
 
 
 if __name__ == '__main__':
